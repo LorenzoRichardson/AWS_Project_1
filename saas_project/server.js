feb203ce23bae77e8c9982608a3a553c36bec22e
EC2 Default User <ec2-user@ip-172-31-16-248.us-east-2.compute.internal>
2025-09-24 23:45:46 +0000

    fixed env

--- saas_project/server.js
+++ saas_project/server.js
@@ -113,6 +113,6 @@ app.use((req, res) => {
 
 // --- START SERVER ---
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
